c36223572c15cec93617a930250d69580a453147
nirel@daedatechnologies.com <nirelmarieibarra@gmail.com>
2025-10-29 11:09:04 +0800

    Routing with Prefixes, Dynamic Segments, and CRUD over In-Memory Data

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -2,9 +2,31 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET Request in Places');
-  res.json({message: 'It works!'});
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Rizal Library",
+    description: "Main library",
+    location: { lat: 14.6406, lng: 121.0747 },
+    address: "Ateneo, QC",
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "Quezon Memorial Circle",
+    description: "QC shrine & park",
+    location: { lat: 14.6515, lng: 121.0537 },
+    address: "Ellipse Rd, QC",
+    creator: "u2",
+  },
+];
+
+router.get('/:pid', (req, res, next) => {
+  const placeId = req.params.pid; // { pid: 'p1' }
+  const place = DUMMY_PLACES.find(p => {
+    return p.id === placeId;
+  });
+  res.json({place}); // => { place } => { place: place }
 });
 
 module.exports = router;
